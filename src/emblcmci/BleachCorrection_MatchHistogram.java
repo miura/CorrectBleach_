0e1d1e417655d675819e3e72685bf47ff68214d4
kotamiura <kotamiura@0ce82a30-2411-ce4f-b84c-be783ca2c29f>
2010-05-07 11:04:13 +0000

    added 3D-time series correction capability.
    the first time point stack is sampled for reference histogram, and all the rest of stacks in each time points are matched to this reference.

--- src/emblcmci/BleachCorrection_MatchHistogram.java
+++ src/emblcmci/BleachCorrection_MatchHistogram.java
@@ -6,6 +6,8 @@ package emblcmci;
  *  
  */
 
+import java.util.Properties;
+
 import histogram2.HistogramMatcher;
 import ij.IJ;
 import ij.ImagePlus;
@@ -15,37 +17,87 @@ import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
 
 public class BleachCorrection_MatchHistogram implements PlugIn { 
-	
+
+
 	public ImagePlus imp = DuplicateStack(WindowManager.getCurrentImage()) ;
 	public void run(String arg) {
+		
+//		Properties properties;
+//		properties = imp.getProperties();
+//		imp.setFileInfo(imp.getOriginalFileInfo());
 		if (imp.getBitDepth()!= 8 && imp.getBitDepth()!=16){
 			IJ.showMessage("should be 8 or 16 bit image");
 			return;
 		}
+			
 		if (imp.getStackSize()<2) {
 			IJ.showMessage("need a stack!");
 			return;
 		}
+		int histbinnum = 0;
+		if (imp.getBitDepth()==8) histbinnum = 256;
+			else if (imp.getBitDepth()==16) histbinnum = 65535;
+		
 		boolean is3DT = false; 
+		int zframes = 1;
+		int timeframes = 1;
 		int[] impdimA = imp.getDimensions();
+		IJ.log("slices"+Integer.toString(impdimA[3])+"  -- frames"+Integer.toString(impdimA[4]));
+		//IJ.log(Integer.toString(imp.getNChannels())+":"+Integer.toString(imp.getNSlices())+":"+ Integer.toString(imp.getNFrames()));
 		if (impdimA[3]>1 && impdimA[4]>1){	// if slices and frames are both more than 1
 			is3DT =true;
-			
+			zframes = impdimA[3];
+			timeframes = impdimA[4];
+			if ((zframes*timeframes) != imp.getStackSize()){
+				IJ.showMessage("slice and time frames do not match with the length of the stack. Please correct!");
+				return;
+			}
 		}
+		
 		ImageStack stack = imp.getStack();
 		ImageProcessor ipA = null;
 		ImageProcessor ipB = null;
 		HistogramMatcher m = new HistogramMatcher();
-		int[] hA = null;
-		int[] hB = null;
-		int[] F = null;
+		int[] hA = new int[histbinnum];
+		int[] hB = new int[histbinnum];
+		int[] F = new int[histbinnum];
+		int[] histB = null;	//for each slice
+		int[] histA = null;
 		//IJ.log(Integer.toString(stack.getSize()));
-		
+		int i =0; 
+		int j =0; 
+		int k =0;
+		/* in case of 3D, histogram from a stack for the first time point is assembled, and then 
+		 *  this stack histogram is used as reference (hB) for the rest of stacks in each time point 
+		 */
 		if (is3DT){
-			//should implement here, 
+			//should implement here,
+			for (i =0; i< timeframes; i++){
+				if (i==0){
+					for (j=0; j<zframes; j++){
+						ipB = stack.getProcessor(i*zframes+j+1);
+						histB = ipB.getHistogram();
+						for (k=0; k<histbinnum; k++) hB[k] += histB[k];
+					}
+				} else {
+					for (k=0; k<histbinnum; k++) hA[k] = 0;
+					for (j=0; j<zframes; j++){
+						ipA = stack.getProcessor(i*zframes+j+1);
+						histA = ipA.getHistogram();
+						for (k=0; k<histbinnum; k++) hA[k] += histA[k];						
+					}					
+					F = m.matchHistograms(hA, hB);
+					for (j=0; j<zframes; j++){
+						ipA = stack.getProcessor(i*zframes+j+1);
+						ipA.applyTable(F);
+					}
+					IJ.log("corrected time point: "+Integer.toString(i+1));
+				}
+			}
+			
 		} else {
 
-			for (int i=0; i<stack.getSize(); i++){
+			for (i=0; i<stack.getSize(); i++){
 				if (i==0) {
 					ipB = stack.getProcessor(i+1);
 					hB = ipB.getHistogram();
@@ -64,13 +116,14 @@ public class BleachCorrection_MatchHistogram implements PlugIn {
 	//duplicate and make another instance of imp
 	public ImagePlus DuplicateStack(ImagePlus ims){
 		ImageStack stack = ims.getImageStack();
-		
+		int[] imsdimA = ims.getDimensions();
 		ImageStack dupstack = ims.createEmptyStack();
 		//ImageStack dupstack = new ImageStack(stack.getWidth(), stack.getHeight());
 		for (int i=0; i<stack.getSize(); i++){
 				dupstack.addSlice(Integer.toString(i), stack.getProcessor(i+1).duplicate(), i); 
 		}
 		ImagePlus dupimp = new ImagePlus("bleach_corrected", dupstack);
+		dupimp.setDimensions(imsdimA[2], imsdimA[3], imsdimA[4]);
 		//dupimp.show();
 		return dupimp;
 	}	
